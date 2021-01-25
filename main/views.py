21c8499563c73a6f89ff44836a27e6f77b221759
islam312 <alyshbekov96@gmail.com>
2021-01-25 18:04:16 +0600

    connection of static files

--- main/views.py
+++ main/views.py
@@ -2,11 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def homepage(request):
-  return HttpResponse("hello world!")
-
+  return render(request,'index.html')
 
 def test(request):
   return render(request, 'test.html')
 
-def test2(request):
-  return HttpResponse('test2 page')
