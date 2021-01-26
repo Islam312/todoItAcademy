3537dac57d16f07fc524da43f15e8eaf48fd8244
islam312 <alyshbekov96@gmail.com>
2021-01-26 11:53:26 +0600

    new models, working with tables DB

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class ToDo(models.Model):
+  describe_text = models.CharField(max_length=50)
+  time_create = models.DateField(auto_now_add=True)
+  is_done = models.BooleanField(default=False)
+  is_important = models.BooleanField(default=False)
