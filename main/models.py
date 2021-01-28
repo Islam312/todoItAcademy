c023fa7cd6091ec0db2466ce91df86317f47ce24
islam312 <alyshbekov96@gmail.com>
2021-01-28 10:41:58 +0600

    create books(models,html), migrations,book request

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.query_utils import subclasses
 
 # Create your models here.
 
@@ -7,3 +8,13 @@ class ToDo(models.Model):
   time_create = models.DateField(auto_now_add=True)
   is_done = models.BooleanField(default=False)
   is_important = models.BooleanField(default=False)
+
+class Books(models.Model):
+  title = models.CharField(max_length=50)
+  subtitle = models.CharField(max_length=50)
+  description = models.CharField(max_length=50)
+  price = models.CharField(max_length=6)
+  genre = models.CharField(max_length=30)
+  author = models.CharField(max_length=30)
+  year = models.CharField(max_length=4)
+  date = models.DateField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import ToDo
+from .models import ToDo, Books
 # Create your views here.
 def homepage(request):
   todo_list = ToDo.objects.all()
@@ -8,4 +8,8 @@ def homepage(request):
 def test(request):
   todo_list = ToDo.objects.all()
   return render(request, 'test.html',{'todo_list': todo_list})
+
+def books(request):
+  books_list = Books.objects.all()
+  return render(request, 'books.html', {'books_list': books_list})
 
